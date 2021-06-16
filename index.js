4021148743c20aec8e232c109a5f7e13165881ff
Leu Family <romanianfobjr@gmail.com>
2021-06-16 12:52:29 -0500

    solved task 1 with guided

--- index.js
+++ index.js
@@ -39,15 +39,46 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+
+  Person.prototype.eat = function(edible){
+    if(this.stomach.length < 10){
+      this.stomach.push(edible);
+    }
+  }
+
+  Person.prototype.poop = function(){
+    this.stomach = [];
+  }
+
+  Person.prototype.toString = function(){
+    return `${this.name}, ${this.age}`;
   }
  
- 
+  const romy = new Person('Romy', 29);
+  const bradly = new Person('Bradly', 27);
+  const jordan = new Person('Jordan', 24);
 
+  // console.log(romy);
+  // console.log(bradly);
+  // console.log(jordan);
   
-  
-  
+  // jordan.eat('taco');
+  // jordan.eat('sandwich');
+  // jordan.eat('egg salad');
+  // jordan.eat('pizza');
+  // jordan.eat('hot dog');
+  // jordan.eat('ramen');
+  // console.log(jordan.stomach);
+
+  // jordan.poop();
+  // console.log(jordan.stomach);
+
+  // console.log(jordan.toString());
   
   /*
     TASK 2
